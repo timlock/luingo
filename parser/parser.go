ea09744bdcac02b5b6d6f68cea832e29880a9e8f
Tim Lock <tim-lock@hotmail.de>
2026-01-31 14:27:11 +0100

    locals WIP

--- parser/parser.go
+++ parser/parser.go
@@ -15,14 +15,15 @@ type Parser struct {
 	stringConstants  map[string]struct{}
 	integerConstants map[int64]struct{}
 	floatConstants   map[float64]struct{}
+	locals           []string
 }
 
 func NewParser(input string) *Parser {
 	return &Parser{
-		lexer:           *lexer.NewLexer(input),
-		stringConstants: map[string]struct{}{},
+		lexer:            *lexer.NewLexer(input),
+		stringConstants:  map[string]struct{}{},
 		integerConstants: map[int64]struct{}{},
-		floatConstants: map[float64]struct{}{},
+		floatConstants:   map[float64]struct{}{},
 	}
 }
 
@@ -40,6 +41,7 @@ func (p *Parser) Parse() ([]vm.Value, []vm.ByteCode, error) {
 		switch token.Type {
 		case lexer.Identifier:
 			p.loadString(token.Str)
+			p.byteCodes = append(p.byteCodes, vm.GetGlobal(0, byte(len(p.byteCodes) -1)))
 
 			next, err := p.lexer.Next()
 			if err != nil {
@@ -52,47 +54,29 @@ func (p *Parser) Parse() ([]vm.Value, []vm.ByteCode, error) {
 				p.byteCodes = append(p.byteCodes, vm.Call(0, 1))
 
 			case lexer.OpenBracket:
-				next, err := p.lexer.Next()
-				if err != nil {
-					return nil, nil, fmt.Errorf("reading function parameter: %w", err)
+				if err := p.loadExpression(); err != nil {
+					return nil, nil, fmt.Errorf("loading expression in function call: %w", err)
 				}
-				switch next.Type {
-				case lexer.Nil:
-					p.byteCodes = append(p.byteCodes, vm.LoadNil(1))
-				case lexer.True:
-					p.byteCodes = append(p.byteCodes, vm.LoadBool(1, true))
-				case lexer.False:
-					p.byteCodes = append(p.byteCodes, vm.LoadBool(1, false))
-				case lexer.Integer:
-					if next.Integer >= 0 && next.Integer <= 65535 {
-						byteCode, err := vm.LoadUInt(1, uint16(next.Integer))
-						if err != nil {
-							return nil, nil, err
-						}
-						p.byteCodes = append(p.byteCodes, byteCode)
-					} else if next.Integer >= -32768 && next.Integer <= 32767 {
-						byteCode, err := vm.LoadInt(1, int16(next.Integer))
-						if err != nil {
-							return nil, nil, err
-						}
-						p.byteCodes = append(p.byteCodes, byteCode)
-					} else {
-						p.loadInteger(next.Integer)
-					}
-				case lexer.Float:
-					p.loadFloat(next.Float)
-				case lexer.String:
-					p.loadString(next.Str)
-				default:
-					return nil, nil, fmt.Errorf("did not expect token %v' as function parameter", next.Type.String())
 
-				}
 				next, err = p.lexer.ExpectToken(lexer.ClosedBracket)
 				if err != nil {
 					return nil, nil, err
 				}
 
 				p.byteCodes = append(p.byteCodes, vm.Call(0, 1))
+
+			case lexer.Local:
+				next, err = p.lexer.ExpectToken(lexer.Assign)
+				if err != nil {
+					return nil, nil, err
+				}
+
+				if err := p.loadExpression(); err != nil {
+					return nil, nil, fmt.Errorf("loading expression assigned to local %v: %w", next.Str, err)
+				}
+
+				p.locals = append(p.locals, next.Str)
+
 			default:
 				return nil, nil, fmt.Errorf("did not expect token %v' after identifier", next.Type.String())
 			}
@@ -104,6 +88,47 @@ func (p *Parser) Parse() ([]vm.Value, []vm.ByteCode, error) {
 	p.constants, p.byteCodes = nil, nil
 
 	return constants, byteCodes, nil
+}
+
+func (p *Parser) loadExpression() error {
+	token, err := p.lexer.Next()
+	if err != nil {
+		return fmt.Errorf("reading function parameter: %w", err)
+	}
+	switch token.Type {
+	case lexer.Nil:
+		p.byteCodes = append(p.byteCodes, vm.LoadNil(1))
+	case lexer.True:
+		p.byteCodes = append(p.byteCodes, vm.LoadBool(1, true))
+	case lexer.False:
+		p.byteCodes = append(p.byteCodes, vm.LoadBool(1, false))
+	case lexer.Integer:
+		if token.Integer >= 0 && token.Integer <= 65535 {
+			byteCode, err := vm.LoadUInt(1, uint16(token.Integer))
+			if err != nil {
+				return err
+			}
+			p.byteCodes = append(p.byteCodes, byteCode)
+		} else if token.Integer >= -32768 && token.Integer <= 32767 {
+			byteCode, err := vm.LoadInt(1, int16(token.Integer))
+			if err != nil {
+				return err
+			}
+			p.byteCodes = append(p.byteCodes, byteCode)
+		} else {
+			p.loadInteger(token.Integer)
+		}
+	case lexer.Float:
+		p.loadFloat(token.Float)
+	case lexer.String:
+		p.loadString(token.Str)
+	case lexer.Identifier:
+		//TODO
+	default:
+		return fmt.Errorf("did not expect token %v' as expression", token.Type.String())
+	}
+
+	return nil
 }
 
 func (p *Parser) loadString(value string) {
